f251f2d395cf0d3d82b15e47417ccb0fa08b27ab
ConnorJohnHager <connorhager12@gmail.com>
2024-01-29 21:23:09 +0000

    Add project files.

---
+++ RandomMenuGenerator/MenuItem.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomMenuGenerator
+{
+    class MenuItem
+    {
+        public static Random Randomizer = new Random();
+
+        public string[] Proteins = { "Roast beef", "Salami", "Turkey", "Ham", "Pastrami", "Tofu" };
+
+        public string[] Condiments = { "yellow mustard", "brown mustard", "honey mustard", "mayo", "relish", "french dressing" };
+
+        public string[] Breads = { "rye", "white", "wheat", "pumpernickel", "a roll" };
+
+        public string Description = "";
+
+        public string Price;
+
+        public void Generate()
+        {
+            string randomProtein = Proteins[Randomizer.Next(Proteins.Length)];
+            string randomCondiment = Condiments[Randomizer.Next(Condiments.Length)];
+            string randomBread = Breads[Randomizer.Next(Breads.Length)];
+            Description = randomProtein + " with " + randomCondiment + " on " + randomBread;
+
+            decimal bucks = Randomizer.Next(2, 5);
+            decimal cents = Randomizer.Next(1, 98);
+            decimal price = bucks + (cents * .01M);
+            Price = price.ToString("c");
+
+        }
+    }
+}
